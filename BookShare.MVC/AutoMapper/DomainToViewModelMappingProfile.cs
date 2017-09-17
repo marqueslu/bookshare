1d4985778bf2fcee37fbb9d1abd474bb5ca2269d
unknown <lucas.msilva09@gmail.com>
2017-09-17 13:28:56 -0300

    Creating the mapping rules

---
+++ BookShare.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BookShare.Domain.Entities;
+using BookShare.MVC.Models;
+
+namespace BookShare.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings";  }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Livro, LivroViewModel > ();
+            Mapper.CreateMap<Autor, AutorViewModel>();
+            Mapper.CreateMap<Categoria, CategoriaViewModel > ();
+        }
+    }
+}
---
+++ BookShare.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BookShare.Domain.Entities;
+using BookShare.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShare.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappinfProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<LivroViewModel, Livro>();
+            Mapper.CreateMap<AutorViewModel, Autor>();
+            Mapper.CreateMap<CategoriaViewModel, Categoria>();
+        }
+    }
+}
