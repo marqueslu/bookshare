3f94bee4e19f80c10dc2d46860d141d077491a93
MICROMIDIA-1\lmarques <lmarques@viceri.com.br>
2017-09-20 17:31:41 -0300

    Criação das views

---
+++ BookShare.MVC/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,86 @@
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(BookShare.MVC.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(BookShare.MVC.App_Start.NinjectWebCommon), "Stop")]
+
+namespace BookShare.MVC.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using Application;
+    using Application.Interface;
+    using Domain.Interfaces.Services;
+    using Domain.Services;
+    using Domain.Interfaces;
+    using Infra.Data.Repositories;
+
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind(typeof(IAppServiceBase<>)).To(typeof(AppServiceBase<>));
+            kernel.Bind<ILivroAppService>().To<LivroAppService>();
+            //kernel.Bind<ICategoriaAppService>().To<CategoriaAppService>();
+            kernel.Bind<IAutorAppService>().To<AutorAppService>();
+
+            kernel.Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
+            kernel.Bind<ILivroService>().To<LivroService>();
+            kernel.Bind<ICategoriaService>().To<CategoriaService>();
+            kernel.Bind<IAutorService>().To<AutorService>();
+
+            kernel.Bind(typeof(IRepositoryBase<>)).To(typeof(RepositoryBase<>));
+            kernel.Bind<ILivroRepository>().To<LivroRepository>();
+            kernel.Bind<ICategoriaRepository>().To<CategoriaRepository>();
+            kernel.Bind<IAutorRepository>().To<AutorRepository>();
+        }
+    }
+}
